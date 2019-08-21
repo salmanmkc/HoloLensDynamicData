f1eaeee78c6d3c2233804d45cffcc5b93f1c29f0
Salman Muin Kayser Chishti <13schishti@gmail.com>
2019-08-21 12:43:10 +0100

    updated deserialisation to work specifically for new request

--- Assets/accessData.cs
+++ Assets/accessData.cs
@@ -29,7 +29,7 @@ public class accessData : MonoBehaviour
                 var response = await client.PostAsync("https://apidae.stage.rco-bozal.uk/graphql", new StringContent(json, Encoding.UTF8, "application/json"));
                 var content = await response.Content.ReadAsStringAsync();
                 var parseddata = JsonConvert.DeserializeObject<parseData.Rootobject>(content);
-                string test = parseddata.data.assets[0].type;
+                //string test = parseddata.data.assets[0].type;
                 string toBreakpoint = "";
             }
 

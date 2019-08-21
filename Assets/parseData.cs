6e2d4aac634a5fc3a8338f68b7b417a9cb4cb2a1
Salman Muin Kayser Chishti <13schishti@gmail.com>
2019-08-21 12:21:56 +0100

    renamed class in document to parseData

--- Assets/parseData.cs
+++ Assets/parseData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class parsingData : MonoBehaviour
+public class parseData : MonoBehaviour
 {
 
     public class Rootobject

6d287c5771395b887ef1a6e41ae2b970fc8b1f57
Salman Muin Kayser Chishti <13schishti@gmail.com>
2019-08-21 12:29:04 +0100

    more specified request, corrected namespaace reference

--- Assets/accessData.cs
+++ Assets/accessData.cs
@@ -20,13 +20,14 @@ public class accessData : MonoBehaviour
         try
         {
             //string example = "{'Username': 'myusername','Password':'pass'}"; 
-            string json = "{\"operationName\":null,\"variables\":{},\"query\":\"{\\n  assets {\\n    uuid\\n    type\\n  }\\n}\\n\"}";
+            //string json = "{\"operationName\":null,\"variables\":{},\"query\":\"{\\n  assets {\\n    uuid\\n    type\\n  }\\n}\\n\"}";
+            string json = "{\"operationName\":null,\"variables\":{},\"query\":\"{\n asset(uuid: \"3c8ca712-6cf8-4ed1-94bf-4cfc1e89b43e\") {\\n    readings(filter: {types: [\"temperature\", \"humidity\"]}) {\\n      type {\\n        name\n      }\\n      values(filter: {limit: 1}) {\\n        values\\n      }\\n    }\\n  }\\n}\\n\"}";
             using (var client = new HttpClient())
             {
                 client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjY4MjAzMTksInVzZXJuYW1lIjoiYWRtaW4ifQ.FMPYbA4s_jghL686qIPEPRliMLnUIJxeLeuVQ763NDk");
                 var response = await client.PostAsync("https://apidae.stage.rco-bozal.uk/graphql", new StringContent(json, Encoding.UTF8, "application/json"));
                 var content = await response.Content.ReadAsStringAsync();
-                var parseddata = JsonConvert.DeserializeObject<parsingData.Rootobject>(content);
+                var parseddata = JsonConvert.DeserializeObject<parseData.Rootobject>(content);
                 string test = parseddata.data.assets[0].type;
             }
 

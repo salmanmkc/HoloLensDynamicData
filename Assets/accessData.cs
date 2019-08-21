3321e11165ccd2a502d478f488deb64c3e46396b
Salman Muin Kayser Chishti <13schishti@gmail.com>
2019-08-21 12:41:29 +0100

    trying again with new request

--- Assets/accessData.cs
+++ Assets/accessData.cs
@@ -21,7 +21,8 @@ public class accessData : MonoBehaviour
         {
             //string example = "{'Username': 'myusername','Password':'pass'}"; 
             //string json = "{\"operationName\":null,\"variables\":{},\"query\":\"{\\n  assets {\\n    uuid\\n    type\\n  }\\n}\\n\"}";
-            string json = "{\"operationName\":null,\"variables\":{},\"query\":\"{\n asset(uuid: \"3c8ca712-6cf8-4ed1-94bf-4cfc1e89b43e\") {\\n    readings(filter: {types: [\"temperature\", \"humidity\"]}) {\\n      type {\\n        name\n      }\\n      values(filter: {limit: 1}) {\\n        values\\n      }\\n    }\\n  }\\n}\\n\"}";
+            //string json = "{\"operationName\":null,\"variables\":{},\"query\":\"{\\n asset(uuid: \"3c8ca712-6cf8-4ed1-94bf-4cfc1e89b43e\") {\\n    readings(filter: {types: [\"temperature\", \"humidity\"]}) {\\n      type {\\n        name\\n      }\\n      values(filter: {limit: 1}) {\\n        values\\n      }\\n    }\\n  }\\n}\\n\"}";
+            string json = "{\"operationName\":null,\"variables\":{},\"query\":\"{\\n asset(uuid: \"3c8ca712-6cf8-4ed1-94bf-4cfc1e89b43e\") {\\n    readings(filter: {types: [\"temperature\", \"humidity\"]}) {\\n      type {\\n        name\\n      }\\n      values(filter: {limit: 1}) {\\n        values\\n      }\\n    }\\n  }\\n}\\n\"}";
             using (var client = new HttpClient())
             {
                 client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjY4MjAzMTksInVzZXJuYW1lIjoiYWRtaW4ifQ.FMPYbA4s_jghL686qIPEPRliMLnUIJxeLeuVQ763NDk");
@@ -29,6 +30,7 @@ public class accessData : MonoBehaviour
                 var content = await response.Content.ReadAsStringAsync();
                 var parseddata = JsonConvert.DeserializeObject<parseData.Rootobject>(content);
                 string test = parseddata.data.assets[0].type;
+                string toBreakpoint = "";
             }
 
             //var request = "https://apidae.stage.rco-bozal.uk/graphql";
